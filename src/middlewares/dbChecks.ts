3b1d0713759452d8fe948860969813cfcd2a3bb9
Tassneem04Hamdy <tassneemhamdy@gmail.com>
2024-01-14 19:57:28 +0200

    chore: unnecessary comment deleted

--- src/middlewares/dbChecks.ts
+++ src/middlewares/dbChecks.ts
@@ -1,26 +1,6 @@
-/* eslint-disable max-len */
 import express from 'express';
 
 import client from '../database/dbClient';
-
-/*
-	||	--function--		||	--value--				||	--error msg--													|| --expected--	||
-	||	addBook				||	req.body.isbn			||	'This book already exists, ISBN:'								||	false		||
-	||	updateBook			||	req.params.bookIsbn		||	'No books found with ISBN:'										||	true		||
-	|| deleteBook			||	req.params.bookIsbn		||	'No books found with ISBN:'										||	true		||
-	||	deleteBook			||	req.params.bookIsbn		||	'Can not delete a book data while it is borrowed, ISBN:'		||	false		||
-	||addBorrower			||	req.body.nid			||	'This Borrower already exists, NID:'							||	false		||
-	||	updateBorrower		||	req.params.borrowerId	||	'No borrowers found with ID:'									||	true		||
-	||	deleteBorrower		||	req.params.borrowerId	||	'No borrowers found with ID:'									||	true		||
-	||	deleteBorrower		||	req.params.borrowerId	||	'Can not delete a borrower that still borrowing book(s), ID:'	||	false		||
-	||	addBorrowing		||	req.params.borrowerId	||	'No borrowers found with ID:'									||	true		||
-	||	returnBook			||	req.params.borrowerId	||	'No borrowers found with ID:'									||	true		||
-	||	returnBook			||	req.params.bookIsbn 	||	'No books found with ISBN:'										||	true		||
-	||	getBorrowedBooks	||	req.params.borrowerId	||	'No borrowers found with ID:'									||	true		||
-
-	const value = req.params.bookIsbn || req.params.borrowerId || req.body.isbn || req.body.nid;
- */
-
 
 export const checkExistence = (fields: string[])  =>
 	async (req: express.Request, res: express.Response, next: express.NextFunction) => {
