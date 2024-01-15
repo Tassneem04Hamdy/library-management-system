e3600c1c76d4febff5d782af321e56a342dd7eb3
Tassneem04Hamdy <tassneemhamdy@gmail.com>
2024-01-15 03:54:31 +0200

    feat: update DB schema

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,10 +1,19 @@
 CREATE DATABASE library_management_system;
 
+CREATE TABLE library (
+	library_id BIGSERIAL NOT NULL PRIMARY KEY,
+	library_name VARCHAR(150) NOT NULL UNIQUE,
+	admin_id BIGSERIAL NOT NULL,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	password VARCHAR(250) NOT NULL
+);
+
 CREATE TABLE book (
 	isbn VARCHAR(100) NOT NULL PRIMARY KEY,
 	title VARCHAR(150) NOT NULL,
 	author VARCHAR(50) NOT NULL,
 	available_quantity INT NOT NULL,
+	library_id BIGINT REFERENCES library(library_id),
 	shelf_location VARCHAR(50) NOT NULL
 );
 
@@ -13,6 +22,7 @@ CREATE TABLE borrower (
 	nid VARCHAR(50) NOT NULL UNIQUE,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(150),
+	library_id BIGINT REFERENCES library(library_id),
     registered_date DATE NOT NULL DEFAULT NOW()::DATE
 );
 
@@ -20,5 +30,6 @@ CREATE TABLE borrowing (
     borrowing_id BIGSERIAL NOT NULL PRIMARY KEY,
 	borrower_id BIGINT REFERENCES borrower(borrower_id),
 	book_isbn VARCHAR(100) REFERENCES book(isbn),
+	library_id BIGINT REFERENCES library(library_id),
 	borrowing_date DATE NOT NULL DEFAULT NOW()::DATE
 );
